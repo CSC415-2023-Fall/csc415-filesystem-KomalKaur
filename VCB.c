ab66420a3c293aa0eec47406808af20d23e19046
LouisHouston <LouisHouston98@gmail.com>
2023-10-28 00:41:19 -0700

    added the vcb to write block0 if

--- VCB.c
+++ VCB.c
@@ -1,8 +1,4 @@
-#include <stddef.h>
-#include <string.h>
-
 #include "VCB.h"
-
 
 int initVCB(uint64_t numberOfBlocks, uint64_t blockSize){
     struct vcb* vcbMain = (struct vcb*)malloc(blockSize); // malloc a pointer to vcb
@@ -10,11 +6,12 @@ int initVCB(uint64_t numberOfBlocks, uint64_t blockSize){
 		perror("Failed to allocate mem for vcb pointer");
 		return 1; // return an error;
 	}
-
 	strcpy(vcbMain->volumeName, ":S"); // Volume Name
 	vcbMain->totalBlocks = numberOfBlocks; // totalBlocks
 	vcbMain->blockSize = blockSize; // blocksize delcaration
 	vcbMain->Signature = SIGNATURE; // signature from hex: 0x4C4F4C41
 	vcbMain->freeBlockCount = numberOfBlocks; // because its empty rn
-	return 0;
+    if(LBAwrite(vcbMain, 1, 0)){
+        return 0;
+    }
 }
--- VCB.h
+++ VCB.h
@@ -1,3 +1,5 @@
+#include <stddef.h>
+#include <string.h>
 #include "fsLow.h"
 
 // Defining 
